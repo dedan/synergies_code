60ed0944e9f57f4976c02414666f7ce479578d82
Stephan Gabler <stephan.gabler@gmail.com>
2010-11-11 16:48:06 +0200

    new runscript to run it for several monkeys at once

---
+++ runall.m
@@ -0,0 +1,4 @@
+
+for monk = {'darma', 'chalva', 'vega'}
+    runscript4emgdat(char(monk))
+end
--- runscript4emgdat.m
+++ runscript4emgdat.m
@@ -1,23 +1,25 @@
-clear
+function runscript4emgdat(monk)
 
-config.path     = '/Volumes/LAB/vega/';
-config.outpath  = '/Volumes/LAB/vega/EMGdat_neu';
-config.monk     = 'vega';
+
+path = '/Volumes/LAB/';
+config.path     = [path monk filesep];
+config.outpath  = [path monk filesep 'EMGdat'];
+config.monk     = monk;
 config.e2add    = 'e';
 config.verbose  = 0;
 
 vdir = dir([config.path 'data' filesep config.monk(1) '*']);
 vdir = sortdirs( vdir);
 
-for i= 2 %1:length(vdir),
-    
+for i= 1 %:length(vdir),
+
     curdir = char(vdir(i));
+    disp(['file ' num2str(i) ' of ' num2str(length(vdir)) ': ' curdir]);
     if ~exist([config.outpath filesep 'EMG' curdir '.mat'], 'file'),
         
-        disp(['file ' num2str(i) ' of ' num2str(length(vdir)) ': ' curdir]);
         [em2take,e1,e2] = findEMGchannels( curdir, config);
         if ~isempty(em2take),
-            [chdata,emgpsth] = MuscleSyn( curdir, e1,e2, em2take, config); 
+            [chdata,emgpsth] = MuscleSyn( curdir, e1,e2, em2take, config);  %#ok<NASGU>
             if ~isempty(chdata),
                 save([config.outpath filesep 'EMG' curdir], 'chdata', 'emgpsth');
                 disp(['save EMG' curdir '.mat to ' config.outpath]);
