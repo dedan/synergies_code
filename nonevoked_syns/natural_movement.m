a9e0c920d8b4416644059964bee4e3949a3a277c
Stephan Gabler <stephan.gabler@gmail.com>
2010-11-16 16:31:02 +0200

    file for the analysis of natural movement
     it is basically the same as the nonevoked syns script, but now for all
     three monkey, some of them don't have both handpositions, and so on. It
     should in the end produce all the data (concerning natural movement)
     which might be needed for an article or poster.
    
     procedures which show the validity of methods (like nmf stability) that
     do not necessarily rely on all data and the latest data will move to
     nonevoked_sessions

---
+++ nonevoked_syns/natural_movement.m
@@ -0,0 +1,11 @@
+
+%% file for the analysis of natural movement
+
+% it is basically the same as the nonevoked syns script, but now for all
+% three monkey, some of them don't have both handpositions, and so on. It
+% should in the end produce all the data (concerning natural movement)
+% which might be needed for an article or poster.
+
+% procedures which show the validity of methods (like nmf stability) that
+% do not necessarily rely on all data and the latest data will move to
+% nonevoked_sessions
