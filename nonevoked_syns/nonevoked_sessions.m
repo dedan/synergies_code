1856ce3b88ff5c347d3cc96ded201fdc94cfd8d8
Stephan Gabler <stephan.gabler@gmail.com>
2010-11-25 17:30:46 +0200

    remove NaNs from channels which are not used anyway

--- nonevoked_syns/nonevoked_sessions.m
+++ nonevoked_syns/nonevoked_sessions.m
@@ -27,17 +27,24 @@ for i= 1:length(fdat)
     end
     for j=1:positions
         mats(j).data_raw = [];
-
+        
         % for all trials
         for k=1:length(data.chdata(j).amp)
             tmp_amp           = data.chdata(j).amp{k};
             tmp_bck           = data.chdata(j).bck_amp{k};
             
             % normalized by division with acitivity before TO
-            mats(j).data_raw        = [mats(j).data_raw; tmp_amp' ./ tmp_bck'];
-            mats(j).data(k,:)       = mean(tmp_amp ./ tmp_bck,2)';
-            mats(j).data_test(k,:)  = mean(tmp_amp,2)' ./ mean(tmp_bck,2)';
+            mats(j).data_raw  = [mats(j).data_raw; tmp_amp' ./ tmp_bck'];
+            mats(j).data(k,:) = mean(tmp_amp,2)' ./ mean(tmp_bck,2)';
         end
+        
+        % remove NaNs from the channels which are not used anyway
+        e2take = logical(data.chdata(1).channels);
+        mats(j).data(:,~e2take) = 0;
+        mats(j).data_raw(:,~e2take) = 0;
+        
+        
+        % warn if there are still NaNs in the data
         if any(isnan(mats(j).data(:))) || any(isnan(mats(j).data_raw(:)))
             disp(['nan problem in: ' fdat(i).name]);
             mats(j).data(isnan(mats(j).data)) = 0;
@@ -55,66 +62,67 @@ for i= 1:length(fdat)
         res(rc).monk     = config.monk;
         res(rc).id       = data.chdata.id;
         res(rc).hands    = length(data.chdata);
-        res(rc).channels = data.chadata(1).channels;
+        res(rc).channels = data.chdata(1).channels;
         
         % initialize test results (to make them all same length)
         vars = {'r_nmf', 'std_nmf', 'r_pca', 'r_nmf_s', ...
-                'r_nmf_raw', 'std_nmf_raw', 'r_pca_raw', 'r_nmf_s_raw'};
+            'r_nmf_raw', 'std_nmf_raw', 'r_pca_raw', 'r_nmf_s_raw'};
         for k = 1:length(vars)
             for l = 1:positions
                 res(rc).([vars{k} config.names{l}]) = zeros(1,config.max_channels);
             end
         end
-
         
-        % explained variance tests 
+        
+        % explained variance tests
         for j = 1:positions
             try
-            [r s]                                               = test_resid_nmf( mats(j).data, config);
-            res(rc).(['r_nmf' config.names{j}])(1:length(r))    = r;
-            res(rc).(['std_nmf' config.names{j}])(1:length(s))  = s;
-            
-            r                                                   = test_resid_pcaica( mats(j).data, config);
-            res(rc).(['r_pca' config.names{j}])(1:length(r))    = r;
-            
-            %repeat for shuffled data as only one random shuffling might not be representative
-            tmp = zeros(config.Niter_res_test, min(size(mats(j).data)));
-            for k = 1:config.Niter_res_test
-                tmp(k,:)    = test_resid_nmf(shuffle_inc(mats(j).data), config);
-            end
-            m = mean(tmp);
-            res(rc).(['r_nmf_s' config.names{j}])(1:length(m))  = m;
-            
-            % and the same for the raw version
-            [r s]                                                   = test_resid_nmf( mats(j).data_raw, config);
-            res(rc).(['r_nmf_raw' config.names{j}])(1:length(r))    = r;
-            res(rc).(['std_nmf_raw' config.names{j}])(1:length(s))  = s;
-            
-            r                                                       = test_resid_pcaica( mats(j).data_raw, config);
-            res(rc).(['r_pca_raw' config.names{j}])(1:length(r))    = r;
-            
-            %repeat for shuffled data as only one random shuffling might not be representative
-            tmp = zeros(config.Niter_res_test, min(size(mats(j).data_raw)));
-            for k = 1:config.Niter_res_test
-                tmp(k,:)    = test_resid_nmf(shuffle_inc(mats(j).data_raw), config);
-            end
-            m = mean(tmp);
-            res(rc).(['r_nmf_s_raw' config.names{j}])(1:length(m))  = m;
-            
+                c2take  = logical(res(rc).channels);
+                [r s]   = test_resid_nmf( mats(j).data(:,c2take), config);
+                res(rc).(['r_nmf' config.names{j}])(1:length(r))    = r;
+                res(rc).(['std_nmf' config.names{j}])(1:length(s))  = s;
+                
+                r       = test_resid_pcaica( mats(j).data(:,c2take), config);
+                res(rc).(['r_pca' config.names{j}])(1:length(r))    = r;
+                
+                %repeat for shuffled data as only one random shuffling might not be representative
+                tmp = zeros(config.Niter_res_test, min(size(mats(j).data(:,c2take))));
+                for k = 1:config.Niter_res_test
+                    tmp(k,:) = test_resid_nmf(shuffle_inc(mats(j).data(:,c2take)), config);
+                end
+                m = mean(tmp);
+                res(rc).(['r_nmf_s' config.names{j}])(1:length(m))  = m;
+                
+                % and the same for the raw version
+                [r s]   = test_resid_nmf( mats(j).data_raw(:,c2take), config);
+                res(rc).(['r_nmf_raw' config.names{j}])(1:length(r))    = r;
+                res(rc).(['std_nmf_raw' config.names{j}])(1:length(s))  = s;
+                
+                r       = test_resid_pcaica( mats(j).data_raw(:,c2take), config);
+                res(rc).(['r_pca_raw' config.names{j}])(1:length(r))    = r;
+                
+                %repeat for shuffled data as only one random shuffling might not be representative
+                tmp = zeros(config.Niter_res_test, min(size(mats(j).data_raw(:,c2take))));
+                for k = 1:config.Niter_res_test
+                    tmp(k,:)    = test_resid_nmf(shuffle_inc(mats(j).data_raw(:,c2take)), config);
+                end
+                m = mean(tmp);
+                res(rc).(['r_nmf_s_raw' config.names{j}])(1:length(m))  = m;
+                
             catch err
                 disp(err.message);
                 disp('no convergence');
-            end 
+            end
         end
         if positions == 1
             res(rc).('r_nmf_sup')        = 0;
             res(rc).('std_nmf_sup')      = 0;
             res(rc).('r_pca_sup')        = 0;
-            res(rc).('r_nmf_s_sup')      = 0;            
+            res(rc).('r_nmf_s_sup')      = 0;
             res(rc).('r_nmf_raw_sup')    = 0;
             res(rc).('std_nmf_raw_sup')  = 0;
             res(rc).('r_pca_raw_sup')    = 0;
-            res(rc).('r_nmf_s_raw_sup')  = 0;            
+            res(rc).('r_nmf_s_raw_sup')  = 0;
         end
         rc = rc +1;
     end
