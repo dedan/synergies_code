082f718edc3ea542cf2ee0d11490077a46273faa
Stephan Gabler <stephan.gabler@gmail.com>
2010-12-05 18:24:18 +0200

    moved all the synergy computation stuff and to
    the computation of the residual values. Made one
    Script out of it. (compute synergies)
    
    removed the pd_consistency file (this is now dann in natural_movement)
    
    removed my circ_std function and now use functions from
    the circular statistics toolbox

---
+++ nonevoked_syns/compute_synergies.m
@@ -0,0 +1,198 @@
+
+% This is a runscript to do all the computationally expensive stuff like
+% residual tests. Once this is run, the files which are created can be used
+% for further analysis
+
+% call this function for example with: 
+% compute_synergies('/Volumes/LAB/', '~/Documents/uni/yifat_lab/results/data/', {'chalva'})
+
+function compute_synergies(data_path, outpath, monks)
+
+addpath('../lib'); %#ok<MCAP>
+
+% general settings
+config = struct;
+config.Niter_exploration= 3;
+config.n_best           = 3;
+config.n_iter_stab      = 3;
+config.Niter_res_test   = 3;
+config.opt              = statset('MaxIter',5);
+config.max_channels     = 16;
+config.modi             = {'_pro','_sup'};
+config.dimensions       = 3;
+
+if config.n_best > config.Niter_exploration
+    disp('n_best has to be smaller than Niter_exploration');
+end
+
+diary([outpath 'log.txt']);
+sessions = struct([]);
+
+for monk = monks
+    config.monk             = char(monk);
+    config.emgdat_path      = [data_path config.monk filesep 'EMGdat' filesep];
+
+    tmp_sess = nonevoked_sessions(config); 
+    if isempty(sessions)
+        sessions = comp_syns(tmp_sess, config);
+    else    
+        sessions = [sessions comp_syns(tmp_sess, config)]; %#ok<AGROW>
+    end
+end
+
+
+save([outpath 'all_data'], 'sessions');
+diary('off');
+disp('finished');
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function sessions = comp_syns(sessions, conf)
+
+% channels which are available for all sessions
+c2take = all(vertcat(sessions.channels));
+
+for i = 1:length(sessions)
+    for j = 1:length(sessions(1).hands)
+        
+        data    = sessions(j).mats(j).data_raw(:,c2take);
+        nmf_res = nmf_explore(data, conf);
+        sessions(j).(['nmf' conf.modi{j}])          = nmf_res.syns;
+        sessions(j).(['nmf' conf.modi{j} '_std'])	= nmf_res.std;
+        sessions(j).(['pca' conf.modi{j}])          = pcaica(data, conf.dimensions)';
+    end
+end
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function res = nonevoked_sessions(config)
+
+% find all sessions and do different residual tests.
+% these are later on used to find out only the interesting sessions (not
+% already explainable by rank1)
+
+res  = struct;
+
+fdat  = dir([config.emgdat_path 'EMG' config.monk(1) '*.mat']);
+rc    = 1;      % result counter
+
+% iterate over all the EMG files
+for i= 1:length(fdat)
+    
+    data = load([config.emgdat_path char(fdat(i).name)]);
+    display(['processing file: ' num2str(i) ' of ' num2str(length(fdat))]);
+    mats = struct;
+    
+    % for pronation and supination (middle position skipped because only
+    % available for a few sessions
+    positions = length(data.chdata);
+    if positions > 2
+        positions = 2;
+    end
+    for j=1:positions
+        mats(j).data_raw = [];
+        
+        % for all trials
+        for k=1:length(data.chdata(j).amp)
+            tmp_amp           = data.chdata(j).amp{k};
+            tmp_bck           = data.chdata(j).bck_amp{k};
+            
+            % normalized by division with acitivity before TO
+            mats(j).data_raw  = [mats(j).data_raw; tmp_amp' ./ tmp_bck'];
+            mats(j).data(k,:) = mean(tmp_amp,2)' ./ mean(tmp_bck,2)';
+        end
+        
+        % remove NaNs from the channels which are not used anyway
+        e2take = logical(data.chdata(1).channels);
+        mats(j).data(:,~e2take) = 0;
+        mats(j).data_raw(:,~e2take) = 0;
+        
+        
+        % warn if there are still NaNs in the data
+        if any(isnan(mats(j).data(:))) || any(isnan(mats(j).data_raw(:)))
+            disp(['nan problem in: ' fdat(i).name]);
+            mats(j).data(isnan(mats(j).data)) = 0;
+            mats(j).data_raw(isnan(mats(j).data_raw)) = 0;
+        end
+    end
+    
+    if isempty(mats(1).data)
+        disp(['problem with: ' fdat(i).name]);
+    else
+        
+        % put in struct what we got so far
+        res(rc).mats     = mats;
+        res(rc).name     = char(fdat(i).name);
+        res(rc).monk     = config.monk;
+        res(rc).hands    = length(data.chdata);
+        res(rc).id       = data.chdata.id;
+        res(rc).channels = data.chdata.channels;
+        res(rc).pd       = data.chdata.pd;
+        res(rc).pd_deg   = data.chdata.pd_deg;
+        res(rc).p1       = data.chdata.p1;
+        res(rc).p2       = data.chdata.p2;
+        
+        % initialize test results (to make them all same length)
+        vars = {'r_nmf', 'std_nmf', 'r_pca', 'r_nmf_s', ...
+            'r_nmf_raw', 'std_nmf_raw', 'r_pca_raw', 'r_nmf_s_raw'};
+        for k = 1:length(vars)
+            for l = 1:positions
+                res(rc).([vars{k} config.modi{l}]) = zeros(1,config.max_channels);
+            end
+        end
+        
+        
+        % explained variance tests
+        for j = 1:positions
+            c2take  = logical(res(rc).channels);
+            [r s]   = test_resid_nmf( mats(j).data(:,c2take), config);
+            res(rc).(['r_nmf' config.modi{j}])(1:length(r))    = r;
+            res(rc).(['std_nmf' config.modi{j}])(1:length(s))  = s;
+            
+            r       = test_resid_pcaica( mats(j).data(:,c2take), config);
+            res(rc).(['r_pca' config.modi{j}])(1:length(r))    = r;
+            
+            %repeat for shuffled data as only one random shuffling might not be representative
+            tmp = zeros(config.Niter_res_test, min(size(mats(j).data(:,c2take))));
+            for k = 1:config.Niter_res_test
+                tmp(k,:) = test_resid_nmf(shuffle_inc(mats(j).data(:,c2take)), config);
+            end
+            m = mean(tmp);
+            res(rc).(['r_nmf_s' config.modi{j}])(1:length(m))  = m;
+            
+            % and the same for the raw version
+            [r s]   = test_resid_nmf( mats(j).data_raw(:,c2take), config);
+            res(rc).(['r_nmf_raw' config.modi{j}])(1:length(r))    = r;
+            res(rc).(['std_nmf_raw' config.modi{j}])(1:length(s))  = s;
+            
+            r       = test_resid_pcaica( mats(j).data_raw(:,c2take), config);
+            res(rc).(['r_pca_raw' config.modi{j}])(1:length(r))    = r;
+            
+            %repeat for shuffled data as only one random shuffling might not be representative
+            tmp = zeros(config.Niter_res_test, min(size(mats(j).data_raw(:,c2take))));
+            for k = 1:config.Niter_res_test
+                tmp(k,:)    = test_resid_nmf(shuffle_inc(mats(j).data_raw(:,c2take)), config);
+            end
+            m = mean(tmp);
+            res(rc).(['r_nmf_s_raw' config.modi{j}])(1:length(m))  = m;
+            
+        end
+        if positions == 1
+            res(rc).('r_nmf_sup')        = 0;
+            res(rc).('std_nmf_sup')      = 0;
+            res(rc).('r_pca_sup')        = 0;
+            res(rc).('r_nmf_s_sup')      = 0;
+            res(rc).('r_nmf_raw_sup')    = 0;
+            res(rc).('std_nmf_raw_sup')  = 0;
+            res(rc).('r_pca_raw_sup')    = 0;
+            res(rc).('r_nmf_s_raw_sup')  = 0;
+        end
+        rc = rc +1;
+    end
+end
+
+
+
+
