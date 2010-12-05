20607a0e0059557ebb8589cb5166addcbd3dc0bf
Stephan Gabler <stephan.gabler@gmail.com>
2010-12-05 18:28:07 +0200

    what is a typical value of a circ_std in our case?

---
+++ method_validations/bootstrp_circ_std.m
@@ -0,0 +1,6 @@
+% I wondered whether the circular std values we get for the pds are high or
+% small and which value is small enough in order to take this channel or is
+% so high that we should reject the channel
+
+uni = rand(1,100000) * 2 * pi;
+circ_std(uni)
