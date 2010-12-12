4aa70ba46437f3ac51fce526653014e566aa8ee7
Stephan Gabler <stephan.gabler@gmail.com>
2010-12-12 12:29:23 +0200

    updated sort_sessions

--- data_validation/sort_sesssions.m
+++ data_validation/sort_sesssions.m
@@ -3,7 +3,7 @@
 % This script is used to sort out the stimulation sessions that contained
 % an artefact. For usage:
 
-% Run the syn_analysis script with the config.debug_images option set to 1
+% Run the inspect_averge_wins script 
 % this will create a average window plot for every stimulation sesssion.
 % place this script into the folder and run it. Then you can rate every
 % response as you like. In the synanalysis you can afterwards sort out
@@ -18,7 +18,9 @@
 % 2 is with an artefact
 % 3 is no response
 
-pics = dir('*.tiff');
+path = '~/Documents/uni/yifat_lab/results/data_validation/';
+
+pics = dir([path '*.tiff']);
 
 for i = 1:length(pics)
     im = imread(pics(i).name);
@@ -26,3 +28,5 @@ for i = 1:length(pics)
     image(im);
     flags(i) = input('wenn ja dann 1: ');
 end
+
+save([path 'handsorted.mat'], flags);
