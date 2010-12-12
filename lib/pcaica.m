c8d299f1224cb1d3607cf0cd9b9a0713010ae123
Stephan Gabler <stephan.gabler@gmail.com>
2010-12-12 17:11:34 +0200

    analysis of settings of the pcaica (runica method)
    it was unstable with this extended 1 setting, so I 
    removed and with this also got rid of an error message.
    
    changed also the pcaica method as I tried to use the runica
    builtin pca. But gives worse results (concerning stability
    over sessions which appears for nmf) and so I used yuvals.
    But still I have to check whether the algo of yuval is correct

--- lib/pcaica.m
+++ lib/pcaica.m
@@ -1,29 +1,26 @@
-function [Wpi,loadings, resid]=pcaica(mat,Ns)
+function Wpi = pcaica(mat,Ns)
 
 %mat = observations X channels
 
+builtin = false;
 
-m=mean(mat);
-lm=size(mat,1);
 
-%compute principle components
-%COEFF - channels X channels
-%SCORE - observations X channels  
-[COEFF, SCORE] = princomp(mat);
-
-%recons=(COEFF(:,1:Ns)*SCORE(:,1:Ns)')'+ones(lm,1)*m;
-
-if Ns>1
-    [W,SP]=runica(SCORE(:,1:Ns)','extended',1, 'verbose', 'off');
-    Wpi=COEFF(:,1:Ns)*inv(W*SP);
-    D=diag(sign(sum(Wpi)));
-    Wpi=Wpi*D;   
-    loadings=inv(D)*(W*SP)*SCORE(:,1:Ns)';
+if builtin
+    [Wpi, ~] = runica(mat', 'pca', 3, 'verbose', 'off');
+    Wpi      = Wpi';
 else
-    Wpi=COEFF(:,1);
-    loadings=SCORE(:,1)';
+        
+    %compute principle components
+    %COEFF - channels X channels
+    %SCORE - observations X channels
+    [COEFF, SCORE] = princomp(mat);
+    
+    if Ns>1
+        [W,SP]=runica(SCORE(:,1:Ns)', 'verbose', 'off');
+        Wpi=COEFF(:,1:Ns)/(W*SP);
+        D=diag(sign(sum(Wpi)));
+        Wpi=Wpi*D;
+    else
+        Wpi=COEFF(:,1);
+    end
 end
-
-SS = sum(sum(mat));
-dif = mat - ones(lm,1)*m-(COEFF(:,1:Ns)*SCORE(:,1:Ns)')';
-resid = (sum(sum(abs(dif)))/SS) * 100;
