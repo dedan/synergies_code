e171c9d433153d9dae4bbc20a0021668d9ccc70a
Stephan Gabler <stephan.gabler@gmail.com>
2010-12-13 14:58:15 +0200

    config umstrukturiert (in syn_analysis)

--- evoked_syns/syn_analysis.m
+++ evoked_syns/syn_analysis.m
@@ -4,95 +4,92 @@ addpath(genpath('../lib')); %#ok<MCAP>
 addpath(genpath('../preprocess')); %#ok<MCAP>
 
 
-%% set configuration
+%% set confuration
 
 clear
 
-config = struct;
+conf = struct;
 
-config.comment = 'will mehr session info in den fin_res struct schreiben';
+conf.comment = 'will mehr session info in den fin_res struct schreiben';
 
-config.monk                     = 'chalva';
-config.modi                     = {'pro', 'sup'};
+conf.monks                    = {'chalva'};
+conf.modi                     = {'pro', 'sup'};
 
 % general settings
-config.sort_out_zerofield       = 1;                                          % sort out sessions with 0 muscle response field
-config.skip_window_computation  = 1;
-config.do_resid_test            = 0;
-config.debug_images             = 0;
-config.inflag                   = true;                                          % write info messages
-config.erflag                   = true;                                          % write error messages
-config.image_format             = 'pdf';                                     % format of images
+conf.sort_out_zerofield       = 1;        % sort out sessions with 0 muscle response field
+conf.skip_window_computation  = 1;
+conf.do_resid_test            = 0;
+conf.inflag                   = true;     % write info messages
+conf.erflag                   = true;     % write error messages
+conf.image_format             = 'pdf';    % format of output images
 
 
 % emg channels
-config.channels            = [11 12 13 14 21 22 23 24 31 32 33 34 41 42 43 44];             % emg channel names
+conf.channels            = [11 12 13 14 21 22 23 24 31 32 33 34 41 42 43 44]; 
 
 % response
-config.stim_value          = 150;                                          % look only at stimulations around this value
+conf.stim_value          = 150;           % look only at stimulations around this value
 
 % matrix factorization
-config.dimensions          = 3;                                                             % reduce to this number of synergies
-config.Niter_res_test      = 10;                                           % number of iterations for the residual test
-config.Niter_exploration   = 50;                                          % number of iterations for nmf exploration
-config.n_best              = 20;
-config.opt                 = statset('MaxIter',50);
+conf.dimensions          = 3;             % reduce to this number of synergies
+conf.Niter_res_test      = 10;            % number of iterations for the residual test
+conf.Niter_exploration   = 50;            % number of iterations for nmf exploration
+conf.n_best              = 20;            % take only n_best explorations
+conf.opt                 = statset('MaxIter',50);     % number of early explorations
 
 
 % files and folders
-config.result_folder       = '~/Documents/uni/yifat_lab/results/';         % path for output
+conf.result_folder       = '~/Documents/uni/yifat_lab/results/';
+conf.cur_res_fold        = [conf.result_folder 'evoked_syns' filesep];
+mkdir(conf.cur_res_fold)
 
 
-% create folder for output of this session
-% config.start_time             = datestr(now, 'ddmmyy_HHMM');
-% config.cur_res_fold           = [config.result_folder config.start_time filesep];
-% mkdir(config.cur_res_fold);
-config.cur_res_fold           = [config.result_folder 'syn_an_debug' filesep];
-mkdir(config.cur_res_fold)
+% sort sessions according to the handsorted flags 
+% (see also ../data_validation/sort_sessions)
+conf.handsorted          = '';
 
-
-% sort sessions again according to the handsorted flags (see also ../response/sort_files)
-config.handsorted          = '';
-
-% write all the output into a file
-diary([config.cur_res_fold 'info.txt']);
-disp(config);
-diary('off');
-diary([config.cur_res_fold 'out.txt']);
-
-disp(['doing analysis for: ' config.monk]);
 
 
 
 %% get responses
 
-if config.skip_window_computation
-    disp('skip window calculation, take data from previous session..');
-else
-    disp('calculate responses .. ');
-    runscript4evoked('/Volumes/LAB/', {config.monk})
+resps = struct([]);
+for monk = conf.monks
+    if conf.skip_window_computation
+        disp('skip window calculation, take data from previous session..');
+    else
+        disp('calculate responses .. ');
+        runscript4evoked('/Volumes/LAB/', monk)
+    end
+    
+    if isemtpy(resps)
+        load([conf.result_folder 'data' filesep 'evoked_data_' char(monk)]);
+    else
+        tmp     = load([conf.result_folder 'data' filesep 'evoked_data_' char(monk)]);
+        resps   = [resps tmp.resps]; %#ok<AGROW>
+    end
 end
-load([config.result_folder 'data' filesep 'evoked_data_' config.monk]);
-
+clear tmp;
 
 
 %% separate and filter responses
 
 
 disp(' ');
-disp(['found ' int2str(length(resps)) ' subsessions in which stimulation took place']);
+disp(['subsessions with stimulation: ' int2str(length(resps))]);
 
 % sort out the sessions which contain artefacts
-load([config.result_folder 'data' filesep config.handsorted 'sort_' config.monk]);
+load([conf.result_folder 'data' filesep conf.handsorted 'sort_' conf.monk]);
 resps = resps(flags ~= 2); 
-disp(['sorted out ' int2str(length(find(flags == 2))) ' subsessions because of artefacts (handsorted)']);
+disp(['sorted out ' int2str(length(find(flags == 2))) ...
+    ' subsessions because of artefacts (handsorted)']);
 disp([int2str(length(resps)) ' subsessions remaining']);
 
 
 % plot the field size histogram
 h = figure('Visible','off');
 hist([resps.field],12);
-saveas(h, [config.cur_res_fold  'resp_field.' config.image_format]);
+saveas(h, [conf.cur_res_fold  'resp_field_' conf.monk '.' conf.image_format]);
 close(h);
 
 % sort out the sessions with fieldsize 0
@@ -109,7 +106,7 @@ end
 % plot the stim amp distribution
 h = figure('Visible','off');
 hist(amps);
-saveas(h, [config.cur_res_fold  'stim_amp_dist.' config.image_format]);
+saveas(h, [conf.cur_res_fold  'stim_amp_dist.' conf.image_format]);
 close(h);
 
 
@@ -119,7 +116,7 @@ fin_res = sep_results_handposition(resps);
 for i = 1:length(fin_res)
     
     disp(' ');
-    disp(config.modi{i});
+    disp(conf.modi{i});
     disp(['number of recordings: ' int2str(length(fin_res(i).dat))]);
 end
 
@@ -140,7 +137,7 @@ for i = 1:length(fin_res)
     hist(fin_res(i).dat(:),100);
     title(['handposition: ' int2str(i)]);
 end
-saveas(h, [config.cur_res_fold 'response_dist' '.' config.image_format]);
+saveas(h, [conf.cur_res_fold 'response_dist' '.' conf.image_format]);
 close(h);
 
 
@@ -149,19 +146,19 @@ close(h);
 
 %% resid tests to see to which dimension data can be reduced
 
-if config.do_resid_test
+if conf.do_resid_test
     
     disp(' ');
     disp('doing resid test ..');
         
     % doing the residual test for nmf
     for i = 1:length(fin_res)
-        resid_res(i,:)  = test_resid_nmf(fin_res(i).dat, config); 
+        resid_res(i,:)  = test_resid_nmf(fin_res(i).dat, conf); 
         
         %repeat for shuffled data as only one random shuffling might not be representative
-        tmp = zeros(config.Niter_res_test, min(size(fin_res(i).dat)));
-        for j = 1:config.Niter_res_test
-            tmp(j,:)    = test_resid_nmf(shuffle_inc(fin_res(i).dat), config);
+        tmp = zeros(conf.Niter_res_test, min(size(fin_res(i).dat)));
+        for j = 1:conf.Niter_res_test
+            tmp(j,:)    = test_resid_nmf(shuffle_inc(fin_res(i).dat), conf);
         end
         resid_resr(i,:) = mean(tmp); %;
         
@@ -173,20 +170,20 @@ if config.do_resid_test
     
     % residual test for pcaica
     for i = 1:length(fin_res)
-        resid_res = [resid_res; test_resid_pcaica(fin_res(i).dat, config)]; %#ok<AGROW>
+        resid_res = [resid_res; test_resid_pcaica(fin_res(i).dat, conf)]; %#ok<AGROW>
 
         %repeat for shuffled data as only one random shuffling might not be representative
-        tmp = zeros(config.Niter_res_test, min(size(fin_res(i).dat)));
-        for j = 1:config.Niter_res_test
-            tmp(j,:)    = test_resid_pcaica(shuffle_inc(fin_res(i).dat), config);
+        tmp = zeros(conf.Niter_res_test, min(size(fin_res(i).dat)));
+        for j = 1:conf.Niter_res_test
+            tmp(j,:)    = test_resid_pcaica(shuffle_inc(fin_res(i).dat), conf);
         end
         resid_resr      = [resid_resr; mean(tmp)]; %#ok<AGROW>
     end
     
     first = ones( (length(fin_res)*2) +1,1)*100; 
     first(length(fin_res)+1) = 10;    
-    h = createfigure1([first resid_res]', [first resid_resr]',config);
-    saveas(h, [config.cur_res_fold 'resid.' config.image_format]);
+    h = createfigure1([first resid_res]', [first resid_resr]',conf);
+    saveas(h, [conf.cur_res_fold 'resid.' conf.image_format]);
     close(h);
     
     disp(' ');
@@ -206,13 +203,13 @@ disp('start the search for synergists..');
 
 for i = 1:length(fin_res)
     
-    nmf_res     = nmf_explore(fin_res(i).dat, config);
-    nmf_res_r   = nmf_explore(shuffle_inc(fin_res(i).dat), config);
+    nmf_res     = nmf_explore(fin_res(i).dat, conf);
+    nmf_res_r   = nmf_explore(shuffle_inc(fin_res(i).dat), conf);
     
     h = figure('Visible','off');
     imagesc(nmf_res.flat);
     title(['standard deviation of group size: ' num2str(nmf_res.std)]);
-    saveas(h, [config.cur_res_fold  'nmf_expl_stab' int2str(i) '.' config.image_format]);
+    saveas(h, [conf.cur_res_fold  'nmf_expl_stab' int2str(i) '.' conf.image_format]);
     close(h);
     disp(['standard deviation of group size: ' num2str(nmf_res.std)]);
     
@@ -229,7 +226,7 @@ disp(' ');
 %% syn analysis with pcaica
 
 for i = 1:length(fin_res)
-    Wpi                  = pcaica(fin_res(i).dat, config.dimensions);
+    Wpi                  = pcaica(fin_res(i).dat, conf.dimensions);
     fin_res(i).pca_syns  = Wpi';
 end;
 disp('finished the search for synergists !');
@@ -242,7 +239,7 @@ for i = 1:length(fin_res)
     
     h = figure('Visible','off');
     nmf_sorting = syn_compare(fin_res(i).pca_syns, fin_res(i).nmf_syns, 'pca', 'nmf');
-    saveas(h, [config.cur_res_fold  'best_syn_relation' int2str(i) '.' config.image_format]);
+    saveas(h, [conf.cur_res_fold  'best_syn_relation' int2str(i) '.' conf.image_format]);
     close(h);
     
     % sort the nmf synergies according to the pca synergies
@@ -257,18 +254,18 @@ clear nmf_sorting
 if length(fin_res) > 1
     h = figure('Visible','off');
     syn_compare(fin_res(1).nmf_syns, fin_res(2).nmf_syns, 'pronation', 'supination');
-    saveas(h, [config.cur_res_fold  'between_pos_syn_relation.' config.image_format]);
+    saveas(h, [conf.cur_res_fold  'between_pos_syn_relation.' conf.image_format]);
     close(h);
     
     h = figure('Visible','off');
     syn_compare(fin_res(1).pca_syns, fin_res(2).pca_syns, 'pronation', 'supination');
-    saveas(h, [config.cur_res_fold  'between_pos_syn_relation_pca.' config.image_format]);
+    saveas(h, [conf.cur_res_fold  'between_pos_syn_relation_pca.' conf.image_format]);
     close(h);
 end
 
-%% save configuration
-save([config.cur_res_fold 'fin_res'], 'fin_res');
-save([config.cur_res_fold 'config'], 'config' );
+%% save confuration
+save([conf.cur_res_fold 'fin_res'], 'fin_res');
+save([conf.cur_res_fold 'conf'], 'conf' );
 diary('off');
 
 disp('finished !');
