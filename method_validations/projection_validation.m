564f1190385ddf4d299bbe7b7a3f80c3f96f9f89
Stephan Gabler <stephan.gabler@gmail.com>
2010-12-27 18:16:28 +0200

    validation of projection method

--- method_validations/projection_validation.m
+++ method_validations/projection_validation.m
@@ -1,10 +1,20 @@
 
-% to test the validity of our projection method
+addpath('../projection/');
 
+
+%% high value expected when it is completely in the spanned subspace
 r = rand(3,11);
 d = repmat(r, 30, 1);
 d = d + 0.05 * randn(size(d));
 
 bla = project(d, r, 1000, 0.5);
-h = plot_proj(bla);
+h   = plot_proj(bla);
+figure(h);
+
+
+%% small value expected for random data in random space
+space   = rand(3,11);
+dat     = rand(100, 11);
+bla = project(dat, space, 1000, 0.5);
+h   = plot_proj(bla);
 figure(h);
