620ce6ec4018cb794598cb5c963c6156e6360e22
Stephan Gabler <stephan.gabler@gmail.com>
2011-01-13 18:36:05 +0200

    fixed explained variance computation in nmf_explore

--- lib/nmf_explore.m
+++ lib/nmf_explore.m
@@ -1,22 +1,23 @@
 function result = nmf_explore(dat, conf)
 
 result = struct;
-col = struct;
+col    = struct;
 
-SS = sum(sum(dat));
+SS     = sum(trace(dat'*dat));
 
 for j = 1:conf.Niter_exploration;
     
     % first run the multiplicative nnmf alorithm mit different
-    % initializations, always only for 5 iterations in order to
-    % explore the search space
-    [W0,H0] = nnmf(dat',conf.dim,'replicates',100,'options',conf.opt,'algorithm','mult');
+    % initializations, always only for the amount of iterations given in opt iterations in 
+    % order to explore the search space
+    [W0,H0]     = nnmf(dat',conf.dim,'replicates',100,'options',conf.opt,'algorithm','mult');
     
     % take best result of exploration episodes and converge using
     % the alternating update rule
-    [W H]  = nnmf(dat', conf.dim,'w0',W0,'h0',H0,'algorithm','als');
+    [W H]       = nnmf(dat', conf.dim,'w0',W0,'h0',H0,'algorithm','als');
     col(j).syns = W';
-    col(j).errs = (sum(sum(abs(dat' - W * H))) / SS) * 100;
+    err         = dat' - W * H;
+    col(j).errs = (sum(trace(err*err')) / SS) * 100;
 end
 
 % sort the error values and choose twenty best results
