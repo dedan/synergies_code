dedan/synergies_code

commit 8490105634f6b04184904e296c3a1e6e2c5231e3

average over trials in nonevoked not computed anymore.
we anyway decided only to use the data_raw field which contains the same just not averaged.

nonevoked_syns/compute_synergies.m
@@ -161,24 +161,21 @@ for i= 1:length(fdat)
             
             % normalized by division with acitivity before TO
             mats(j).data_raw  = [mats(j).data_raw; tmp_amp' ./ tmp_bck'];
-            mats(j).data(k,:) = mean(tmp_amp,2)' ./ mean(tmp_bck,2)';
         end
         
         % remove NaNs from the channels which are not used anyway
         e2take = logical(data.chdata(1).channels);
-        mats(j).data(:,~e2take) = 0;
         mats(j).data_raw(:,~e2take) = 0;
         
         
         % warn if there are still NaNs in the data
         if any(isnan(mats(j).data(:))) || any(isnan(mats(j).data_raw(:)))
             disp(['nan problem in: ' fdat(i).name]);
-            mats(j).data(isnan(mats(j).data)) = 0;
             mats(j).data_raw(isnan(mats(j).data_raw)) = 0;
         end
     end
     
-    if isempty(mats(1).data)
+    if isempty(mats(1).data_raw)
         disp(['problem with: ' fdat(i).name]);
     else
         

commit a879cabbdc9a86a8b1a734d2a686455a000cf938

fix a bug introduced in 8490105634f6b04
here we decided to only use the data_raw (responses not averaged over trials). I did not remove all the code properly

also remove the target statistics

because I don't remember anymore what it really means and I think it also does not make sense if we only use the data_raw struct which is not averaged over trials.

nonevoked_syns/compute_synergies.m
@@ -49,6 +49,7 @@ disp('finished');
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% estimate the baseline of the matching scores
 function stats = estimate_baseline(sessions, conf)
 
 m_scores = NaN(length(sessions), conf.n_baseline);
@@ -169,7 +170,7 @@ for i= 1:length(fdat)
         
         
         % warn if there are still NaNs in the data
-        if any(isnan(mats(j).data(:))) || any(isnan(mats(j).data_raw(:)))
+        if any(isnan(mats(j).data_raw(:)))
             disp(['nan problem in: ' fdat(i).name]);
             mats(j).data_raw(isnan(mats(j).data_raw)) = 0;
         end
@@ -204,22 +205,6 @@ for i= 1:length(fdat)
         % explained variance tests
         for j = 1:positions
             c2take  = logical(res(rc).channels);
-            [r s]   = test_resid_nmf( mats(j).data(:,c2take), config);
-            res(rc).(['r_nmf' config.modi{j}])(1:length(r))    = r;
-            res(rc).(['std_nmf' config.modi{j}])(1:length(s))  = s;
-            
-            r       = test_resid_pcaica( mats(j).data(:,c2take), config);
-            res(rc).(['r_pca' config.modi{j}])(1:length(r))    = r;
-            
-            %repeat for shuffled data as only one random shuffling might not be representative
-            tmp = zeros(config.Niter_res_test, min(size(mats(j).data(:,c2take))));
-            for k = 1:config.Niter_res_test
-                tmp(k,:) = test_resid_nmf(shuffle_inc(mats(j).data(:,c2take)), config);
-            end
-            m = mean(tmp);
-            res(rc).(['r_nmf_s' config.modi{j}])(1:length(m))  = m;
-            
-            % and the same for the raw version
             [r s]   = test_resid_nmf( mats(j).data_raw(:,c2take), config);
             res(rc).(['r_nmf_raw' config.modi{j}])(1:length(r))    = r;
             res(rc).(['std_nmf_raw' config.modi{j}])(1:length(s))  = s;

nonevoked_syns/nonevoked_analysis.m
@@ -68,14 +68,7 @@ clear tmp stats i
 % First lets get some statistics on the Data we have available
 
 for i = 1:length(sessions)
-    sessions(i).target1     = size(sessions(i).mats(1).data,1); 
     sessions(i).n_channels  = length(find(sessions(i).channels)); 
-    
-    if length(sessions(i).mats) == 2
-        sessions(i).target2    = size(sessions(i).mats(2).data,1); 
-    else
-        sessions(i).target2    = -1; 
-    end
 end
 
 h = figure('Visible', 'off');
@@ -85,17 +78,6 @@ for i = 1:conf.n_monks
     idx.(conf.names{i})    = strcmp(conf.names{i}, {sessions.monk});
     
     disp([conf.names{i} ': ' num2str(length(find(idx.(conf.names{i})))) ' sessions' ]);
-
-    % distribution of targets
-    subplot(4,conf.n_monks,i);
-    [y x] = hist([sessions(idx.(conf.names{i})).target1],-1:8);
-    bar(x,y,'r');
-    hold on
-    [y x] = hist([sessions(idx.(conf.names{i})).target2],-1:8);
-    bar(x+0.2,y,'b');
-    hold off
-    title(conf.names{i});
-    xlabel('targets');
 
     % distribution of used channels
     subplot(4, conf.n_monks, conf.n_monks +i);
@@ -148,7 +130,6 @@ for i = 1:conf.n_monks
     idx.(monk)(idx.(monk)) = idx.(monk)(idx.(monk)) & out < conf.n_pd_unstable;
 end
 clear data all_mean all_std i inds j monk out 
-
 
 
 %% only use channels which are available for all sessions of a monk