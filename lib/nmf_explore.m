5df757f77f02a4b1dd3290452d89697539ee18cd
Stephan Gabler <stephan.gabler@gmail.com>
2012-02-23 17:57:47 +0200

    also fix the explained variance measure in nmf_explore

--- lib/nmf_explore.m
+++ lib/nmf_explore.m
@@ -3,21 +3,22 @@ function result = nmf_explore(dat, conf)
 result = struct;
 col    = struct;
 
-SS     = sum(trace(dat'*dat));
+centered = matin - repmat(mean(matin), size(matin, 1), 1);
+SS = sum(sum(centered.^2));
 
 for j = 1:conf.Niter_exploration;
-    
+
     % first run the multiplicative nnmf alorithm mit different
-    % initializations, always only for the amount of iterations given in opt iterations in 
+    % initializations, always only for the amount of iterations given in opt iterations in
     % order to explore the search space
     [W0,H0]     = nnmf(dat',conf.dim,'replicates',100,'options',conf.opt,'algorithm','mult');
-    
+
     % take best result of exploration episodes and converge using
     % the alternating update rule
     [W H]       = nnmf(dat', conf.dim,'w0',W0,'h0',H0,'algorithm','als');
     col(j).syns = W';
     err         = dat' - W * H;
-    col(j).errs = (sum(trace(err*err')) / SS) * 100;
+    col(j).errs = (sum(sum(err.^2)) / SS) * 100;
 end
 
 % sort the error values and choose twenty best results
