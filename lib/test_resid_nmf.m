6b692306b6afb26f8563e587fba3ad9629e7eb0f
Stephan Gabler <stephan.gabler@gmail.com>
2012-02-23 17:43:23 +0200

    fix the measure for explained variance
    in the previous version I forgot to subtract the mean.
    
    http://en.wikipedia.org/wiki/Fraction_of_variance_unexplained

--- lib/test_resid_nmf.m
+++ lib/test_resid_nmf.m
@@ -6,18 +6,17 @@ function [v var_res]=test_resid_nmf( matin , config)
 % structured data, espacially for the nonevoked data
 warning off stats:nnmf:LowRank
 
-% sum of squares
-SS = sum(trace(matin'*matin));
+centered = matin - repmat(mean(matin), size(matin, 1), 1);
+SS = sum(sum(centered.^2));
 v = NaN(config.Niter_exploration, min(size(matin)));
 for i=1:min(size(matin))
     for j=1:config.Niter_exploration
-        [w,h]   = nnmf(matin',i);
-        D       = matin' - w*h;
-        v(j,i)  = sum(trace(D'*D));
+        [w,h]=nnmf(matin',i);
+        D = matin' - w*h;
+        v(j,i) = sum(sum(D.^2));
     end
 end
 var_res = std(v);
 v = (min(v)/SS) * 100;
 
 warning on stats:nnmf:LowRank
-    
--- lib/test_resid_pcaica.m
+++ lib/test_resid_pcaica.m
@@ -2,7 +2,8 @@ function v=test_resid_pcaica( matin, config)
 
 % simple residual test
 
-SS = sum(sum(matin));
+centered = matin - repmat(mean(matin), size(matin, 1), 1);
+SS = sum(sum(centered.^2));
 m  = mean(matin);
 lm = size(matin,1);
 
@@ -12,6 +13,6 @@ v = zeros(1,min(size(matin)));
 for i=1:min(size(matin))
     % mean has to be used for normalization, is removed during princomp
    dif = matin - ones(lm,1)*m - (w(:,1:i)*h(:,1:i)')';
-   v(i) = sum(sum(abs(dif)));
+   v(i) = sum(sum(dif.^2));
 end
 v = (v/SS) * 100;
