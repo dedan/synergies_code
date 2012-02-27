badaff4d35be3287bfe96f8b9d406b0520aa4c69
Stephan Gabler <stephan.gabler@gmail.com>
2012-02-27 20:10:46 +0200

    remove the principal angle stuff from the projection analysis

--- projection/proj_analysis.m
+++ projection/proj_analysis.m
@@ -23,11 +23,11 @@ for i = 1:length(conf.monks)
     % load the nonevoked results
     tmp     = load([conf.res_folder 'data' filesep 'nat_mov_res_' monk '.mat']);
     nat_mov_res.(monk) = tmp.nat_mov_res;
-    
+
     if isempty(resps)
-        load([conf.res_folder 'data' filesep 'evoked_data_' monk '.mat']);     
+        load([conf.res_folder 'data' filesep 'evoked_data_' monk '.mat']);
     else
-        tmp     = load([conf.res_folder 'data' filesep 'evoked_data_' monk '.mat']);     
+        tmp     = load([conf.res_folder 'data' filesep 'evoked_data_' monk '.mat']);
         resps   = [resps tmp.resps]; %#ok<AGROW>
     end
 end
@@ -37,13 +37,10 @@ end
 load([conf.res_folder 'data' filesep 'evoked_res.mat']);
 
 
-%% all evoked on all nonevoked 
-
 % shows that the evoked responses tend to reside in the same subspace as
 % spanned by the synergies found during natural movement
-
 for m = conf.monks
-    
+
     monk = char(m);
     
     idx         = strcmp(monk, {resps.monk});
@@ -51,50 +48,7 @@ for m = conf.monks
     responses   = vertcat(resps(larger).response);
 
     proj_res = project(responses, nat_mov_res.(monk).synall_pro, conf.n_boot, conf.noise);
-    
     h = plot_proj(proj_res);
-    
     saveas(h, [conf.out 'projection' filesep 'projection_larger0_' monk '.' conf.image_format]);
     close(h);
-        
-
 end
-
-
-
-%% principal angles between clusters and synergies
-
-
-for m = conf.monks
-    
-    monk = char(m);
-    disp(monk);
-    disp('pro pro');
-    disp(num2str(subspace(evoked_res.(monk).pro.center', nat_mov_res.(monk).synall_pro')));
-    
-    if ~isempty(nat_mov_res.(monk).synall_sup)
-        disp('sup sup');
-        disp(num2str(subspace(evoked_res.(monk).pro.center', nat_mov_res.(monk).synall_pro')));
-    end
-end
-
-n = 10000;
-r = zeros(1,n);
-d = 11;
-ds = 3;
-
-for i = 1:n
-    r(i) = subspace(rand(d, ds), rand(d,ds));
-end
-figure
-hist(r,100)
-
-
-
-
-
-
-
-
-
-    
