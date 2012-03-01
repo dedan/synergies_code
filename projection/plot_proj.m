675c41a99a83e23b57869381c2dff334b16f4c8f
Stephan Gabler <stephan.gabler@gmail.com>
2012-03-01 17:09:37 +0200

    go back to the old version (second_draft_version) of proj_analysis
    don't distinguish between m1 and non m1

--- projection/plot_proj.m
+++ projection/plot_proj.m
@@ -1,4 +1,4 @@
-function h = plot_proj(in, m1flag)
+function h = plot_proj(in)
 
 % a function to plot the results of a projection anaylisis and labels it.
 % labelling has to be changed when for example test data is switched on in
@@ -7,7 +7,6 @@ function h = plot_proj(in, m1flag)
 h = figure('Visible', 'off');
 
 bins = 100;
-m1flag = logical(m1flag);
 
 ratios      = NaN(4,length(in.shuf_ratios));
 ratios(1,:) = in.rand_ratios;
@@ -23,23 +22,13 @@ for i = 1:size(ratios,1)
     hold on
     [n, xout] = hist(ratios(i,:),bins);
     bar(xout, n / size(ratios,2)  , 'b');
-    plot(mean(in.ratio_dist) * ones(1,10), linspace(0,max(n / size(ratios,2)), 10), '*b');
-    plot(mean(in.ratio_dist(m1flag)) * ones(1,10), linspace(0,max(n / size(ratios,2)), 10), '*r');
+    plot(mean(in.ratio_dist) * ones(1,10), linspace(0,max(n / size(ratios,2)), 10), '*r');
     hold off;
     xlim([0 max([mean(ratios(1,:)) mean(in.ratio_dist)]) + 2 * std(in.ratio_dist)]);
 end
 
 subplot(5,1,1);
-m1ratio = in.ratio_dist(m1flag);
-pmratio = in.ratio_dist(~m1flag);
-[~, p] = ttest2(m1ratio, pmratio)
-
-[n_m1, x_m1] = hist(m1ratio, bins);
-[n_pm, x_pm] = hist(pmratio, bins);
-n = sum(n_m1) + sum(n_pm);
-bar(x_pm, n_pm ./ n, 'b')
-hold on
-bar(x_m1, n_m1 ./ n, 'r')
-hold off
+[n,xout] = hist(in.ratio_dist, bins);
+bar(xout, n / length(in.ratio_dist), 'r');
 xlim([0 max([mean(ratios(1,:)) mean(in.ratio_dist)]) + 2 * std(in.ratio_dist)]);
-title(['distribution of projection ratios n=' num2str(length(in.ratio_dist)) '(m1 = ' num2str(sum(m1flag)) ')']);
+title('distribution of projection ratios')
--- projection/proj_analysis.m
+++ projection/proj_analysis.m
@@ -20,16 +20,14 @@ resps       = struct([]);
 for i = 1:length(conf.monks)
     monk = conf.monks{i};
 
-    add_mapinfo([conf.res_folder 'data' filesep], {monk})
-
     % load the nonevoked results
     tmp     = load([conf.res_folder 'data' filesep 'nat_mov_res_' monk '.mat']);
     nat_mov_res.(monk) = tmp.nat_mov_res;
 
     if isempty(resps)
-        load([conf.res_folder 'data' filesep 'evoked_data_map_' monk '.mat']);
+        load([conf.res_folder 'data' filesep 'evoked_data_' monk '.mat']);
     else
-        tmp     = load([conf.res_folder 'data' filesep 'evoked_data_map_' monk '.mat']);
+        tmp     = load([conf.res_folder 'data' filesep 'evoked_data_' monk '.mat']);
         resps   = [resps tmp.resps]; %#ok<AGROW>
     end
 end
@@ -39,28 +37,26 @@ end
 load([conf.res_folder 'data' filesep 'evoked_res.mat']);
 
 
+%% all evoked on all nonevoked
+
 % shows that the evoked responses tend to reside in the same subspace as
 % spanned by the synergies found during natural movement
-for i=1:length(resps),
-    m1flag(i) = 0;
-    if strcmp(resps(i).mapsite(1), 'm1') &  strcmp(resps(i).mapsite(2), 'm1'),
-        m1flag(i) = 1; % this is m1
-    end
-end
-projall = zeros(length(resps), 1);
 
 for m = conf.monks
 
     monk = char(m);
 
     idx         = strcmp(monk, {resps.monk});
-    larger      = find(idx & [resps.field] > 0);
+    larger      = idx & [resps.field] > 0;
     responses   = vertcat(resps(larger).response);
 
     proj_res = project(responses, nat_mov_res.(monk).synall_pro, conf.n_boot, conf.noise);
-    projall(larger) = proj_res.ratio_dist;
-    h = plot_proj(proj_res, m1flag(larger));
+
+    h = plot_proj(proj_res);
+
     saveas(h, [conf.out 'projection' filesep 'projection_larger0_' monk '.' conf.image_format]);
     close(h);
+
+
 end
 
